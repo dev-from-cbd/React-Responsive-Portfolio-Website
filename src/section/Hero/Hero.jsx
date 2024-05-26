08c64d3cc3144f78c6c4659b16a0b615e51e651b
dev-from-cbd <veryfatwombat@gmail.com>
2024-05-27 07:49:36 +0930

    React JS Project

--- src/section/Hero/Hero.jsx
+++ src/section/Hero/Hero.jsx
@@ -1,7 +1,37 @@
 import styles from "./HeroStyles.module.css";
+import heroImg from "../../assets/oleg.png";
+import themeIcon from "../../assets/sun.svg";
+import twitterLight from "../../assets/twitter-light.svg";
 
 function Hero() {
-  return <div>React Responsive Portfolio Website</div>;
+  return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          className={styles["rounded-image"]}
+          src={heroImg}
+          alt="Profile image of the Web Developer"
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Junior
+          <br /> Web Developer
+        </h1>
+        <h2>Oleg Grid</h2>
+        <span>
+          <a href="https://x.com" target="_blank">
+            <img src={twitterLight} alt="X.Com" />
+          </a>
+        </span>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
