eb849b9209c1a56f12ded7542fa9587173492388
dev-from-cbd <veryfatwombat@gmail.com>
2024-05-31 07:43:39 +0930

    Projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Hero from "./section/Hero/Hero";
+import Projects from "./section/Projects/Projects";
 
 function App() {
   return (
     <>
       <Hero />
+      <Projects />
     </>
   );
 }
--- src/section/Hero/Hero.jsx
+++ src/section/Hero/Hero.jsx
@@ -1,14 +1,23 @@
 import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/oleg.png";
-import themeIcon from "../../assets/sun.svg";
-import twitterIcon from "../../assets/twitter-light.svg";
-import githubIcon from "../../assets/github-light.svg";
-import linkedinIcon from "../../assets/linkedin-light.svg";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import twitterLight from "../../assets/twitter-light.svg";
+import twitterDark from "../../assets/twitter-dark.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
 import CV from "../../assets/cv.pdf";
 import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
   const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === "light" ? sun : moon;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
 
   return (
     <section id="hero" className={styles.container}>
@@ -32,13 +41,21 @@ function Hero() {
         </h1>
         <h2>Oleg Grid</h2>
         <span>
-          <a href="https://github.com/dev-from-cbd" target="_blank">
+          <a
+            href="https://github.com/dev-from-cbd"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src={githubIcon} alt="GitHub" />
           </a>
-          <a href="https://linkedin.com" target="_blank">
+          <a
+            href="https://linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src={linkedinIcon} alt="linkedIn" />
           </a>
-          <a href="https://x.com" target="_blank">
+          <a href="https://x.com" target="_blank" rel="noopener noreferrer">
             <img src={twitterIcon} alt="X.Com" />
           </a>
         </span>
@@ -47,8 +64,8 @@ function Hero() {
           I've developed numerous valuable solutions that significantly benefit
           your business!
         </p>
-        <a href={CV}>
-          <button className="hover" download>
+        <a href={CV} rel="noopener noreferrer">
+          <button className={styles.hover} download>
             Resume
           </button>
         </a>
