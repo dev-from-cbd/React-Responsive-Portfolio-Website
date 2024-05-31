ce7d0c7da6d8ace5cb9dc97f1b9a4dae69c1b352
dev-from-cbd <veryfatwombat@gmail.com>
2024-06-01 07:33:37 +0930

    Projects

--- src/section/Projects/Projects.jsx
+++ src/section/Projects/Projects.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import styles from "./ProjectsStyles.module.css";
+
 
 function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <a href="https://dev-from-cbd.github.io/Lawn-Care-Website-JS-HTML-CSS/">
+          <img classNamesrc="" alt="" />
+        </a>
+      </div>
+    </section>
+  );
 }
 
-export default Projects
+export default Projects;
