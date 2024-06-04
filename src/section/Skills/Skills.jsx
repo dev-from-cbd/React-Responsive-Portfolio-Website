7a5381be4c585255c7309bca450a62aa16633971
dev-from-cbd <veryfatwombat@gmail.com>
2024-06-05 06:58:43 +0930

    Skills

--- src/section/Skills/Skills.jsx
+++ src/section/Skills/Skills.jsx
@@ -4,6 +4,11 @@ function Skills() {
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
+      <div className={styles.skillList}>
+        <span>
+          <img src="Skills" alt="Skills" />
+        </span>
+      </div>
     </section>
   );
 }
