a06b3cc0189ae398606755e2b6255e6854a31866
dev-from-cbd <veryfatwombat@gmail.com>
2024-06-18 06:30:52 +0930

    Contact

--- src/section/Contact/Contact.jsx
+++ src/section/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import styles from "./ContactStyles.module.css";
 
 function Contact() {
-  return <section>Contact</section>;
+  return <section id="contact">Contact</section>;
 }
 
 export default Contact;
